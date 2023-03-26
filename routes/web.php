acb9bcaa24bfa01716c3f79f575337062aed43ad
dantheman <danogbo0@gmail.com>
2023-03-26 13:22:54 +0100

    mailable wetup complete commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Mail\WelcomeMail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/email', function () {
+    return new WelcomeMail();
 });
